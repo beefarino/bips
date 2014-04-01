dbaa58a9498bbcd074d2d0fc51d6468e46c1e8f7
beefarino <jimchristopher@gmail.com>
2014-03-31 23:17:31 -0400

    added format files

--- src/CodeOwls.BIPS.Provider/Properties/AssemblyInfo.cs
+++ src/CodeOwls.BIPS.Provider/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CodeOwls.BIPS.Provider")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2014 Code Owls LLC, All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
