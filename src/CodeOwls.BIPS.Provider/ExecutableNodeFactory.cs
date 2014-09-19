ca9fe0479b286ac415aa7057536f903eabec8ee6
beefarino <jimchristopher@gmail.com>
2014-09-19 13:23:44 -0400

    note properties for executable types

--- src/CodeOwls.BIPS.Provider/ExecutableNodeFactory.cs
+++ src/CodeOwls.BIPS.Provider/ExecutableNodeFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Automation;
 using System.Runtime.InteropServices;
 using CodeOwls.PowerShell.Provider.PathNodeProcessors;
 using CodeOwls.PowerShell.Provider.PathNodes;
@@ -12,48 +13,69 @@ namespace CodeOwls.BIPS
     public class ExecutableNodeFactory : NodeFactoryBase
     {
         private readonly DtsContainer _executable;
+        private readonly PSObject _psExecutable;
+        private readonly TaskHost _host;
+        private readonly Sequence _seq;
+        private readonly ForEachLoop _foreachloop;
+        private readonly ForLoop _forloop;
+        private readonly MainPipe _mainPipe;
 
         public ExecutableNodeFactory(Executable executable)
         {
             _executable = (DtsContainer)executable;
-            
+            _host = _executable as TaskHost;
+            _seq = _executable as Sequence;
+            _foreachloop = _executable as ForEachLoop;
+            _forloop = _executable as ForLoop;
+            _psExecutable = PSObject.AsPSObject(_executable);
+
+            if (null != _host)
+            {
+                _psExecutable.Properties.Add( new PSNoteProperty( "IsTaskHost", true ));
+                _mainPipe = _host.InnerObject as MainPipe;
+            }
+            if (null != _mainPipe)
+            {
+                _psExecutable.Properties.Add(new PSNoteProperty("IsDataFlow", true));
+            }
+            if (null != _seq)
+            {
+                _psExecutable.Properties.Add(new PSNoteProperty("IsSequence", true));
+            }
+            if (null != _foreachloop)
+            {
+                _psExecutable.Properties.Add(new PSNoteProperty("IsForEachLoop", true));
+            }
+            if (null != _forloop)
+            {
+                _psExecutable.Properties.Add(new PSNoteProperty("IsForLoop", true));
+            }
         }
 
         public override IEnumerable<INodeFactory> GetNodeChildren(IContext context)
         {
             var nodes = new List<INodeFactory>();
-
-            var host = _executable as TaskHost;
-            var seq = _executable as Sequence;
-            var foreachloop = _executable as ForEachLoop;
-            var forloop = _executable as ForLoop; 
-            MainPipe mainPipe = null;
-
-            if (null != host)
+           
+            if (null != _mainPipe)
             {
-                mainPipe = host.InnerObject as MainPipe;
-            }
-            
-            if (null != mainPipe)
-            {
-                var metadata = mainPipe.ComponentMetaDataCollection.Cast<IDTSComponentMetaData100>();
+                var metadata = _mainPipe.ComponentMetaDataCollection.Cast<IDTSComponentMetaData100>();
                 nodes.AddRange(metadata.ToList().ConvertAll(c => new DataFlowComponentNodeFactory(c)));
             }
-            else if (null != host)
+            else if (null != _host)
             {
-                nodes.Add( new TaskNodeFactory(host.InnerObject as Task) );
+                nodes.Add( new TaskNodeFactory(_host.InnerObject as Task) );
             }
-            else if (null != seq)
+            else if (null != _seq)
             {
-                nodes.AddRange(seq.Executables.Cast<Executable>().ToList().ConvertAll( e=> new ExecutableNodeFactory(e)));
+                nodes.AddRange(_seq.Executables.Cast<Executable>().ToList().ConvertAll( e=> new ExecutableNodeFactory(e)));
             }
-            else if (null != foreachloop)
+            else if (null != _foreachloop)
             {
-                nodes.AddRange(foreachloop.Executables.Cast<Executable>().ToList().ConvertAll(e => new ExecutableNodeFactory(e)));
+                nodes.AddRange(_foreachloop.Executables.Cast<Executable>().ToList().ConvertAll(e => new ExecutableNodeFactory(e)));
             }
-            else if (null != forloop)
+            else if (null != _forloop)
             {
-                nodes.AddRange(forloop.Executables.Cast<Executable>().ToList().ConvertAll(e => new ExecutableNodeFactory(e)));
+                nodes.AddRange(_forloop.Executables.Cast<Executable>().ToList().ConvertAll(e => new ExecutableNodeFactory(e)));
             }
 
             return nodes;
@@ -61,7 +83,7 @@ namespace CodeOwls.BIPS
 
         public override IPathNode GetNodeValue()
         {
-            return new ContainerPathNode( _executable, Name);
+            return new ContainerPathNode( _psExecutable, Name);
         }
 
         public override string Name
